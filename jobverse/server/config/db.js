536bb268e1aa9596bc96339e1cdfecce0ff85f9c
Tran Ha Anh Duy <trananhduydl@gmail.com>
2025-09-23 20:54:11 +0700

    Update dbURL

--- jobverse/server/config/db.js
+++ jobverse/server/config/db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-
+require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || "postgres://postgres:abc123@localhost:5432/db_web_tim_kiem_viec_lam",
