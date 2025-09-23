9a692700205b858210f4f1ec59320ca25b26d3e2
Tran Ha Anh Duy <trananhduydl@gmail.com>
2025-09-23 15:25:12 +0700

    update homepage

--- jobverse/client/src/index.js
+++ jobverse/client/src/index.js
@@ -7,7 +7,7 @@ import "./style/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/job_searching_website">
         <RouterCustom/>
     </BrowserRouter>
 );
