f9a1d7f53465161e802dc4fb9ce4ded138f5551b
Tran Ha Anh Duy <trananhduydl@gmail.com>
2025-09-23 22:51:04 +0700

    Update config/db

--- jobverse/server/config/db.js
+++ jobverse/server/config/db.js
@@ -1,9 +1,25 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgres://postgres:abc123@localhost:5432/db_web_tim_kiem_viec_lam",
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
-});
+let config;
+
+if (process.env.DATABASE_URL) {
+  // Cloud DB (Render)
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  };
+} else {
+  // Localhost DB
+  config = {
+    user: "postgres",
+    password: "abc123",
+    host: "localhost",
+    port: 5432,
+    database: "db_web_tim_kiem_viec_lam"
+  };
+}
+
+const pool = new Pool(config);
 
 module.exports = pool;
